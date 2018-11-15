55db426c30b09651c150bf10f7752f069019c7b4
qiuzhaopeng <qiuzhaopeng@feiliu.com>
2018-11-16 02:17:31 +0800

    第二版

--- js/index.js
+++ js/index.js
@@ -46,3 +46,21 @@ function countDown(time) {
         $(".countDown").text(`${time}`)
     }, 1000)
 }
+
+var flag = true
+$(".drag").click(function () {
+    if (flag == true) {
+        $(".drag").animate({ right: "8.8rem" }, 1000, function () {
+            // $(".drag").text("》")
+            flag = false
+        })
+        $("iframe").animate({ right: "0" }, 1000)
+    } else {
+        $(".drag").animate({ right: "0.2rem" }, 1000, function () {
+            // $(".drag").text("《")
+            flag = true
+        })
+        $("iframe").animate({ right: "-8.8rem" }, 1000) 
+    }
+
+})
